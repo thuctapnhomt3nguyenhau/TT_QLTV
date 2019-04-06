180bc7ef5783d049811e38110a5c4ce0102a2b58
pham mai anh <maianh1204@gmail.com>
2019-04-06 22:47:14 +0700

    nhân viên

---
+++ QLTV/QLTV/DAO/NhanVienDAO.cs
@@ -0,0 +1,63 @@
+﻿using QLTV.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV.DAO
+{
+    class NhanVienDAO
+    {
+        private static NhanVienDAO instance;
+
+        internal static NhanVienDAO Instance
+
+        {
+            get { if (instance == null) instance = new NhanVienDAO(); return instance; }
+            private set { instance = value; }
+        }
+        public List<NhanVienDTO> NVList { get; private set; }
+        public List<NhanVienDTO> GetNV()
+        {
+            List<NhanVienDAO> list = new List<NhanVienDAO>();
+            Datatable data = DataProvider.Instance.ExecuteQuery("dbo.USP_GETNV ");
+            foreach (DataRow item in data.Rows)
+            {
+                NhanVienDTO nv = new NhanVienDTO(item);
+                list.Add(nv);
+            }
+            return list;
+        }
+        public bool INSERTNV(string hoTen, DateTime ngaySinh, string gioiTinh)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery(" EXEC USP_INSERTSACH @HOTEN , @NGAYSINH , @GIOITINH ", new object[] { hoTen , ngaySinh , gioiTinh });
+
+            return result > 0;
+        }
+        public bool UPDATENV(int maNV, string hoTen, DateTime ngaySinh, string gioiTinh)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery(" EXEC USP_UPDATENV @MANV , @HOTEN , @NGAYSINH , @GIOITINH ", new object[] { maNV , hoTen , ngaySinh , gioiTinh });
+
+            return result > 0;
+        }
+        public bool DELETENV(int maNV)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery(" EXEC USP_DELETENV @MASACH ", new object[] { maNV });
+
+            return result > 0;
+        }
+        public List<NhanVienDTO> SEARCHNV(string str)
+        {
+            List<NhanVienDTO> NVList = new List<NhanVienDTO>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("EXEC USP_SEARCHSACH @search ", new object[] { str });
+            foreach (DataRow item in data.Rows)
+            {
+                NhanVienDTO nv = new NhanVienDTO(item);
+                NVList.Add(nv);
+            }
+            return NVList;
+        }
+    }
+}
---
+++ QLTV/QLTV/DTO/NhanVienDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLTV.DTO
+{
+    class NhanVienDTO
+    {
+        private int maNV;
+        private string hoTen;
+        private DateTime ngaySinh;
+        private string gioiTinh;
+
+        public int MaNV { get => maNV; set => maNV = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public DateTime NgaySinh { get => ngaySinh; set => ngaySinh = value; }
+        public string GioiTinh { get => gioiTinh; set => gioiTinh = value; }
+
+        public NhanVienDTO( int maNV,string hoTen,DateTime ngaySinh,string gioiTinh)
+        {
+            this.maNV = maNV;
+            this.hoTen = hoTen;
+            this.ngaySinh = ngaySinh;
+            this.gioiTinh = gioiTinh;
+        }
+        public NhanVienDTO(DataRow row)
+        {
+            Int32.TryParse(row["MANV"].ToString(), out this.maNV);
+            this.hoTen = row["HOTEN"].ToString();
+            this.ngaySinh = (DateTime)row["NGAYSINH"];        
+            this.gioiTinh = row["GIOITINH"].ToString();     
+        }
+    }
+}
